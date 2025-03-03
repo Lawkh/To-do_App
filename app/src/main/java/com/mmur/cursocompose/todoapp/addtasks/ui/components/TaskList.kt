92cbe3384da33259e70e416ca73a7720d3077020
Marcelo Mur <mmur@hiberus.com>
2025-03-03 13:54:21 +0100

    Added list with simpler call

--- app/src/main/java/com/mmur/cursocompose/todoapp/addtasks/ui/components/TaskList.kt
+++ app/src/main/java/com/mmur/cursocompose/todoapp/addtasks/ui/components/TaskList.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
@@ -31,11 +32,8 @@ fun TasksList(myTasks: List<TaskModel>) {
     LazyColumn(
         modifier = Modifier.fillMaxSize().testTag("task_list")
     ) {
-        items(
-            count = myTasks.size,
-            key = { index -> myTasks[index].id },
-            contentType = { "task" }) {
-            TaskItem(myTasks[it]  )
+        items(myTasks){
+            TaskItem(it)
         }
     }
 }
